41f216a35b18bcec20d6165d0c59cfc9cfd26b6a
Princess Irabor <irabor.princess1@gmail.com>
2019-06-27 01:13:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,11 @@ def long_planeteer_calls(words)# code an argument here
   greater_than_four
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheeses)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheeses.each do |thischeese|
+    cheese_types.include?(thischeese)
+  end
+  
 end
