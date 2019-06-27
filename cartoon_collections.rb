3a9992b287bdcd8164f6143f1fae97a27884074b
Princess Irabor <irabor.princess1@gmail.com>
2019-06-27 01:18:39 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,38 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves) # code an argument here
   # Your code here
+  dwarves.each_with_index do |thisdwarf,index|
+    puts "#{index+1} #{thisdwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planet_calls)# code an argument here
   # Your code here
+  planet_calls.collect do |thisplanet_call|
+    thisplanet_call.capitalize + "!"
+  
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(words)# code an argument here
   # Your code here
+  greater_than_four = false 
+  words.each do |thisword|
+     if thisword.length > 4
+       greater_than_four = true
+     end
+  end
+  greater_than_four
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheeses)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  foundcheese = nil
+  cheeses.each do |thischeese|
+    if cheese_types.include?(thischeese)
+      foundcheese = thischeese
+      return foundcheese
+    end
+  end
+  foundcheese
 end
