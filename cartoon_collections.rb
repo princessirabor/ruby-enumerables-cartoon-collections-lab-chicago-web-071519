aa5031056b4f855b0c546cfa92b6a25b1d401fcf
Princess Irabor <irabor.princess1@gmail.com>
2019-06-27 00:27:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,11 @@ def roll_call_dwarves(dwarves) # code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planet_calls)# code an argument here
   # Your code here
+  planet_calls.collect do |thisplanet_call
+  
+  end
 end
 
 def long_planeteer_calls# code an argument here
